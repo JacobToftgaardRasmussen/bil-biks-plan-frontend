c9618710f45ede11e4f6a6a56917ded52e267927
Jacob Toftgaard Rasmussen <1jatora@gmail.com>
2023-11-25 09:46:24 +0400

    request to get all vehicles

--- app/src/app/vehicle.tsx
+++ app/src/app/vehicle.tsx
@@ -1,3 +1,34 @@
+"use client";
+import { useState } from "react";
+
+interface Vehicle {
+  licensePlate: String;
+  make: String;
+  model: String;
+  year: number;
+  trimLevel?: String;
+  priceClass: number;
+}
+
 export default function Vehicle() {
-  return <div>Vehicle</div>;
+  const [allVehicles, setAllVehicles] = useState<Vehicle[]>([]);
+  async function getAllVehicles() {
+    const response = await fetch("http://localhost:5000/vehicle/all");
+    const data = await response.json();
+    setAllVehicles(data);
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>Get all vehicles</h2>
+        <button onClick={getAllVehicles}>Go!</button>
+        <ul>
+          {allVehicles.map((v, i) => {
+            return <li key={i}>{v.licensePlate}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
