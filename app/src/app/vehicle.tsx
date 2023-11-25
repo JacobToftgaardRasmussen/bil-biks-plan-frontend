295869c302a105f15abd8c6f5d979b5b13e0fabe
Jacob Toftgaard Rasmussen <1jatora@gmail.com>
2023-11-25 10:02:39 +0400

    clear vehicle results and added daisy UI

--- app/src/app/vehicle.tsx
+++ app/src/app/vehicle.tsx
@@ -12,18 +12,29 @@ interface Vehicle {
 
 export default function Vehicle() {
   const [allVehicles, setAllVehicles] = useState<Vehicle[]>([]);
+  const [showVehicles, setshowVehicles] = useState<boolean>(false);
   async function getAllVehicles() {
     const response = await fetch("http://localhost:5000/vehicle/all");
     const data = await response.json();
     setAllVehicles(data);
+    setshowVehicles(true);
+  }
+  function clearResults() {
+    setshowVehicles(false);
   }
 
   return (
     <div>
       <div>
         <h2>Get all vehicles</h2>
-        <button onClick={getAllVehicles}>Go!</button>
-        <ul>
+        <button onClick={getAllVehicles} className="btn btn-primary">
+          Go!
+        </button>
+        <button onClick={clearResults} className="btn btn-secondary">
+          Clear results
+        </button>
+
+        <ul hidden={!showVehicles}>
           {allVehicles.map((v, i) => {
             return <li key={i}>{v.licensePlate}</li>;
           })}
